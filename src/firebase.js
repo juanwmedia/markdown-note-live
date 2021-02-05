606220456ace08be87b7abf24c3906b133e2277a
juanwmedia <juan@wmedia.es>
2021-02-05 17:27:17 +0100

    Agregar Firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: "markdowmnoteslive.firebaseapp.com",
+  projectId: "markdowmnoteslive",
+  storageBucket: "markdowmnoteslive.appspot.com",
+  messagingSenderId: "1001515354210",
+  appId: "1:1001515354210:web:c7fd9b746c1dbfc66ea93b"
+};
+
+firebase.initializeApp(firebaseConfig);
